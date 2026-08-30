da459bb6fadcade4372b3bb5fc9b5e566c7f6b30
samarthupa <samarthu100@gmail.com>
2026-08-30 00:20:36 -0700

    dd

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  build: {
+    assets: 'assets'
+  },
   vite: {
     plugins: [tailwindcss()]
   }
