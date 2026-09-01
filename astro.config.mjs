49f47ee16fe803dc2864bad25343c4a5bde64f12
samarthupa <samarthu100@gmail.com>
2026-09-01 08:56:18 -0700

    ff

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import react from '@astrojs/react';
 import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  output: 'server', // 👈 This is mandatory for Better Auth & Middleware routing
+  output: 'static', // 👈 This is mandatory for Better Auth & Middleware routing
   adapter: cloudflare({
     platformProxy: { enabled: true },
   }),
