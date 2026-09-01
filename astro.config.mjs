a56a762bc4fb9ff03fb81fa5a4cec13a68835ae8
samarthupa <samarthu100@gmail.com>
2026-09-01 08:56:45 -0700

    ff

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,9 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
-import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  output: 'static', // 👈 This is mandatory for Better Auth & Middleware routing
-  adapter: cloudflare({
-    platformProxy: { enabled: true },
-  }),
+  output: 'static', 
   build: {
     assets: 'assets'
   },
